66652d5e0cda2b5ea0d42193f55a1d04f03842df
unknown <b81123tw@gmail.com>
2024-06-19 20:11:11 +0800

    Add app.py file

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 16 14:33:14 2024
-
-@author: 88693
-"""
-
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.exceptions import InvalidSignatureError
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import logging
 
 app = Flask(__name__)
 
 # LINE 設定
-LINE_CHANNEL_ACCESS_TOKEN = '3N+hwqKvNK8MUNVrV4EeboNrD/1liUCyq30MZ241s1BDNKRie2hMywwMBquG72uqVFdQZqL+/TcqsO5sD2De5abt841S4Fb79Y0khqBCIe1jWsVW/JTxlabWkjoYdFfa8zcxOuPhJnjVIb3SJ+fhGwdB04t89/1O/w1cDnyilFU='
-LINE_CHANNEL_SECRET = '93a8d88c1459c647e0a80e3ea21d95b8'
+LINE_CHANNEL_ACCESS_TOKEN = 'YOUR_CHANNEL_ACCESS_TOKEN'
+LINE_CHANNEL_SECRET = 'YOUR_CHANNEL_SECRET'
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+# 設定日誌
+logging.basicConfig(level=logging.INFO)
 
 # 建立 ChatterBot 聊天機器人
 chatbot = ChatBot('LearningBot')
@@ -28,13 +26,14 @@ trainer.train('chatterbot.corpus.english')
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
+    signature = request.headers.get('X-Line-Signature')
     body = request.get_data(as_text=True)
     app.logger.info(f"Request body: {body}")
 
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        app.logger.error("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
@@ -42,19 +41,27 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_message = event.message.text
+    app.logger.info(f"User message: {user_message}")
 
-    if user_message == '你好':
-        reply_message = '你好！'
-    elif user_message == '再見':
-        reply_message = '再見，祝你有個美好的一天！'
-    else:
-        bot_response = chatbot.get_response(user_message)
-        reply_message = str(bot_response)
+    try:
+        if user_message == '你好':
+            reply_message = '你好！'
+        elif user_message == '再見':
+            reply_message = '再見，祝你有個美好的一天！'
+        else:
+            bot_response = chatbot.get_response(user_message)
+            reply_message = str(bot_response)
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply_message)
-    )
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_message)
+        )
+    except Exception as e:
+        app.logger.error(f"Error handling message: {e}")
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="對不起，我遇到了一些問題，請稍後再試。")
+        )
 
 if __name__ == "__main__":
     app.run()
